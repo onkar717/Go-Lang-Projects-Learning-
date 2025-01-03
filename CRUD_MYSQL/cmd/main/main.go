4e5c6b9c16fb48b60f1f322d7522e5fe121a23ae
onkar717 <onkarraghunathshelke.com>
2025-01-03 21:29:31 +0530

    2nd project

---
+++ CRUD_MYSQL/cmd/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"github.com/gorilla/mux"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/onkar717/pkg/routes"
+)
+
+func main()  {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+}
